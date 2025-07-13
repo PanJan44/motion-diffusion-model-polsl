2ca726f8c76bba540dc06be3e9b09f2c9cb2c645
Janek <jasjanek33@gmail.com>
2025-07-13 13:30:38 +0200

    one more debug info

--- diffusion/gaussian_diffusion.py
+++ diffusion/gaussian_diffusion.py
@@ -1297,6 +1297,7 @@ class GaussianDiffusion:
                 ModelMeanType.EPSILON: noise,
             }[self.model_mean_type]
             assert model_output.shape == target.shape == x_start.shape  # [bs, njoints, nfeats, nframes]
+            debug(f"Model output: {model_output}")
 
             terms["rot_mse"] = self.masked_l2(target, model_output, mask) # mean_flat(rot_mse)
 
